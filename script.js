d6d695b3bf75d0eb7054ac7430b58acd7f6522a7
Ana Sander <anagsander@gmail.com>
2022-08-09 11:54:07 -0300

    JavaScript inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+// LINKS ATIVOS NO MENU
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
